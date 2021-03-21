20e3656ab2bd8670b5fa2ce3a3416b4e702aa6c2
Leslie <75267383+lescardone@users.noreply.github.com>
2021-03-20 20:34:15 -0400

    Install Alfred
    Use --no-quarantine option (for other casks too)

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -2,10 +2,11 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install httpie
 brew install bat
 
-brew install --cask google-chrome
-brew install --cask visual-studio-code
+brew install --cask --no-quarantine google-chrome
+brew install --cask --no-quarantine visual-studio-code
+brew install --cask --no-quarantine alfred
